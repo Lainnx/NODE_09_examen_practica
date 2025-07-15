1b1c3a14e53ffa5162fe5dc35510d3d064088ac7
das <tree@gmail.com>
2025-07-15 20:00:37 +0200

    2025/07/15

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 // Importar las dependencias
 import express from "express";
 import {connection} from "./mysql/mysql.js"
+import send from "send";
+import { connect } from "http2";
 
 // Obtener el puerto del servidor
 process.loadEnvFile()
@@ -11,12 +13,46 @@ console.log(PORT);
 // Crear la aplicación
 const app = express();
 
+//Donde esta la ruta de los ficheros estaticos/ public
+app.use(express.static(process.cwd() + "/public"))
+
 // RUTAS
 
 app.get("/",(req,res)=>{
-
+    // res.send("Aquí irá la página inicial")
+    res.sendFile("/index.html")
 })
 
+
+app.get("/alumnos",(req,res)=>{
+    const query ="SELECT * from alumno;"
+    // es el connection que importamos de arriba, err obligatorio para manejar error, result lo que devolvera, field campo optativo
+    connection.query(query,(err, result)=>{
+        if(err) throw err /*levanta el mensaje de error y para el programa*/
+        // console.log(result);
+        res.json(result) // devuelve json con resultado del select
+    })
+})
+
+app.get("/alumnos/:iniciales",(req,res)=>{
+    const query2 = `SELECT * from alumno WHERE apellido1 LIKE "${req.params.iniciales}%" ORDER BY apellido1, apellido2;` /*IMPORTANTE COMILLAS*/
+    
+    connection.query(query2,(err,result)=>{
+        // console.log(result);
+        if(err) throw err
+        res.json(result)
+    })
+})
+
+app.get("/alumnos/:iniciales1/:iniciales2",(req,res)=>{
+    const query3 = `SELECT * FROM alumno WHERE apellido1 LIKE "${req.params.iniciales1}%" and apellido2 LIKE "${req.params.iniciales2}%";`
+
+    connection.query(query3,(err,result)=>{
+        // console.log(result);
+        if(err) throw err
+        res.json(result)
+    })
+})
 
 
 app.listen(PORT, ()=>{
---
+++ sql/script_universidad.sql
@@ -0,0 +1,3 @@
+use facultad;
+
+SELECT 
